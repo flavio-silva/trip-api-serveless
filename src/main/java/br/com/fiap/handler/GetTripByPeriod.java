b65d8f4826bf3cfa4a60f5906766f5b82ec20c2a
Rafael <rafaelmiranda178@gmail.com>
2020-04-18 18:28:48 -0300

    Finished GetTripByPeriod

--- src/main/java/br/com/fiap/handler/GetTripByPeriod.java
+++ src/main/java/br/com/fiap/handler/GetTripByPeriod.java
@@ -4,15 +4,12 @@ import br.com.fiap.dao.TripRepository;
 import br.com.fiap.model.HandlerRequest;
 import br.com.fiap.model.HandlerResponse;
 import br.com.fiap.model.Trip;
-import com.amazonaws.services.dynamodbv2.model.AttributeValue;
 import com.amazonaws.services.lambda.runtime.Context;
 import com.amazonaws.services.lambda.runtime.RequestHandler;
 
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 
-public class GetTripBetweenDates implements RequestHandler<HandlerRequest, HandlerResponse> {
+public class GetTripByPeriod implements RequestHandler<HandlerRequest, HandlerResponse> {
 
 	private final TripRepository repository = new TripRepository();
 
