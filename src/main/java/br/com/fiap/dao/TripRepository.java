73604816988b54770b06ba4e796a0dc40100bc7c
Flavio Silva <flavioalvesfsilva@gmail.com>
2020-04-22 00:51:39 -0300

    fix find by period with scan expression instead

--- src/main/java/br/com/fiap/dao/TripRepository.java
+++ src/main/java/br/com/fiap/dao/TripRepository.java
@@ -3,8 +3,8 @@ package br.com.fiap.dao;
 import br.com.fiap.model.Trip;
 import com.amazonaws.services.dynamodbv2.datamodeling.DynamoDBMapper;
 import com.amazonaws.services.dynamodbv2.datamodeling.DynamoDBQueryExpression;
+import com.amazonaws.services.dynamodbv2.datamodeling.DynamoDBScanExpression;
 import com.amazonaws.services.dynamodbv2.model.AttributeValue;
-
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -12,33 +12,34 @@ import java.util.UUID;
 
 public class TripRepository {
 
-    private static final DynamoDBMapper mapper = DynamoDbManager.mapper();
+    private static DynamoDBMapper mapper = DynamoDbManager.mapper();
 
-    public List<Trip> findByPeriod(String starts, String ends) {
-        final Map<String, AttributeValue> eav = new HashMap<String, AttributeValue>();
-        eav.put(":starts", new AttributeValue().withS(starts));
-        eav.put(":ends", new AttributeValue().withS(ends));
+    public List<Trip> findByPeriod(String start, String end) {
+        Map<String, AttributeValue> eav = new HashMap<>();
+        eav.put(":start", new AttributeValue().withS(start));
+        eav.put(":end", new AttributeValue().withS(end));
 
-        final DynamoDBQueryExpression<Trip> queryExpression = new DynamoDBQueryExpression<Trip>()
-                .withKeyConditionExpression("dateTrip between :starts and :ends")
-                .withExpressionAttributeValues(eav);
+        Map<String, String> expression = new HashMap<>();
+        expression.put("#date", "date");
 
-        return mapper.query(Trip.class, queryExpression);
+        DynamoDBScanExpression scanExpression = new DynamoDBScanExpression()
+                .withFilterExpression("#date between :start and :end")
+                .withExpressionAttributeValues(eav)
+                .withExpressionAttributeNames(expression);
 
+        return mapper.scan(Trip.class, scanExpression);
     }
 
     public List<Trip> findById(String id) {
 
-        final Map<String, AttributeValue> eav = new HashMap<String, AttributeValue>();
+        Map<String, AttributeValue> eav = new HashMap<>();
         eav.put(":id", new AttributeValue().withS(id));
 
-        final DynamoDBQueryExpression<Trip> queryExpression = new DynamoDBQueryExpression<Trip>()
+        DynamoDBQueryExpression<Trip> queryExpression = new DynamoDBQueryExpression<Trip>()
                 .withKeyConditionExpression("id = :id")
                 .withExpressionAttributeValues(eav);
 
-        final List<Trip> trips = mapper.query(Trip.class, queryExpression);
-
-        return trips;
+        return mapper.query(Trip.class, queryExpression);
     }
 
     public Trip save(Trip trip) {
